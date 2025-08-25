233a3cd108b77cb13e29a6b3b82e89d12b11cbbb
Vamshi0111 <vamshichary117@gmail.com>
2025-08-25 15:04:19 +0530

    pushing

---
+++ src/app/screens/services/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Header from "@/components/Header/page";
+
+
+function Page() {
+    return (
+        <>
+            <div className="lg:h-[100vh] flex flex-col justify-start">
+                <div className="lg:h-[9%] sticky top-0 z-50 shadow-lg">
+                    <Header />
+                </div>
+                <div className="flex flex-col bg-yellow-400 lg:p-4 overflow-y-auto">
+                    Its services page
+                </div>
+            </div>
+        </>
+    )
+}
+export default Page;
