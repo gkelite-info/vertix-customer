0d0c097b43f03ee831c60fbc7cf969da708bb2a5
gkelite-info <gkeliteinfo@gmail.com>
2025-10-13 12:22:09 +0530

    pushing

--- src/app/(pageNavbar)/mainLayout/page.tsx
+++ src/app/(pageNavbar)/mainLayout/page.tsx
@@ -1,33 +1,24 @@
 "use client"
 
-import { useRouter, useSearchParams } from "next/navigation"
+import { useRouter, useSearchParams } from 'next/navigation'
 import PageNavbar from "../pageNavbar"
-import VertixTaxPage from "../VertixDocUpload/page"
-import ReferAFriend from "../ReferAFriend/page"
-import Feedback from "../Feedback1/page"
-import { Suspense } from "react"
 import ManageFilingYear from "../ManageFilingYear/page"
 import FileProgressTracker from "../FileProgressTracker/page"
 import TaxPreparationGuide from "../(TaxPreparationGuide)/TaxPreparationGuide"
+import VertixTaxPage from "../VertixDocUpload/page"
 import PaymentTaxSummary from "../PaymentTaxSummary/page"
-import BankingInformation from "../BankingInformation/page"
+// import BankingInformation from "../BankingInformation/page"
 import MyDocuments from "../MyDocuments/page"
+import ReferAFriend from "../ReferAFriend/page"
 import Chats from "../Chats/page"
+import Feedback from "../Feedback1/page"
 import AuthorizationConsent from "../AuthorizationConsent/page"
 
-function MainLayout() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Content />
-    </Suspense>
-  )
-}
-
-export default function Content() {
+export default function ClientMainLayout() {
   const router = useRouter()
   const searchParams = useSearchParams()
 
-  const activeTab = searchParams.get("tab") || "service"
+  const activeTab: string = searchParams.get("tab") || "service"
 
   const setActiveTab = (tab: string) => {
     const params = new URLSearchParams(searchParams.toString())
@@ -36,9 +27,9 @@ export default function Content() {
   }
 
   return (
-    <div className="bg-green-00 flex justify-between">
+    <div className="flex justify-between">
       <PageNavbar activeTab={activeTab} setActiveTab={setActiveTab} />
-      <div className="bg-red-00 w-[76%]">
+      <div className="w-[76%]">
         {activeTab === "service" && <ManageFilingYear />}
         {activeTab === "file-status" && <FileProgressTracker />}
         {activeTab === "organizer" && <TaxPreparationGuide />}
