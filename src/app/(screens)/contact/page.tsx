329cabf6ebc11c250fd8aee8dc4042af2c9cc09e
gkelite-info <gkeliteinfo@gmail.com>
2025-10-17 12:08:42 +0530

    integrated with contact page and data is storing in supabase

--- src/app/(screens)/contact/page.tsx
+++ src/app/(screens)/contact/page.tsx
@@ -1,134 +1,46 @@
-// function Page() {
-//     return (
-//         <main>
-//             {/* Page Title */}
-//             <div className="bg-white text-white py-8">
-//                 <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center px-4">
-//                     <h1 className="text-3xl font-bold text-[#1D2B48]">Contact</h1>
-//                 </div>
-//             </div>
+'use client'
 
-//             {/* Contact Section */}
-//             <section id="contact" className="py-0 bg-white lg:pb-10">
-//                 {/* Map */}
-//                 <div className="mb-10">
-//                     <iframe
-//                         className="w-full h-[400px] rounded-lg shadow-md"
-//                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7612.986577123368!2d78.44641198813004!3d17.436088483837402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb90b7b28eced7%3A0x53434bd3914f4737!2sGreenland%20Towers!5e0!3m2!1sen!2sin!4v1755668697654!5m2!1sen!2sin"
-//                         allowFullScreen
-//                         loading="lazy"
-//                         referrerPolicy="no-referrer-when-downgrade"
-//                     ></iframe>
-//                 </div>
+import { insertContact } from "@/app/api/SupabaseAPI/contactApi";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
-//                 <div className="container mx-auto px-4">
-//                     <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
-//                         {/* Contact Info */}
-//                         <div className="space-y-6 shadow-2xl pt-5 rounded-lg">
-//                             <div>
-//                                 <h3 className="text-xl font-semibold text-[#1D2B48] text-center">Get in touch</h3>
-//                                 <p className="text-black text-center">For any concerns, please contact us.</p>
-//                             </div>
+export default function Page() {
 
-//                             <div className="flex items-start gap-3 pl-2">
-//                                 <span className="text-indigo-600 text-xl">📍</span>
-//                                 <div>
-//                                     <h4 className="font-semibold text-[#1D2B48]">Location:</h4>
-//                                     <p className="text-[#1D2B48]">
-//                                         Begumpet,
-//                                         Hyderabad, Telangana, 500016.
-//                                     </p>
-//                                 </div>
-//                             </div>
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
 
-//                             <div className="flex items-start gap-3 pl-2">
-//                                 <span className="text-indigo-600 text-xl">✉️</span>
-//                                 <div>
-//                                     <h4 className="font-semibold text-[#1D2B48]">Email:</h4>
-//                                     <p className="text-[#1D2B48]">Vertixtaxsolutions@gmail.com</p>
-//                                 </div>
-//                             </div>
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-//                             <div className="flex items-start gap-3 pl-2">
-//                                 <span className="text-indigo-600 text-xl">📞</span>
-//                                 <div>
-//                                     <h4 className="font-semibold text-[#1D2B48]">Call:</h4>
-//                                     <p className="text-[#1D2B48]">
-//                                         +91 9898989898 <br /> +91 7070707070
-//                                     </p>
-//                                 </div>
-//                             </div>
-//                         </div>
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-//                         {/* Contact Form */}
-//                         <div className="lg:col-span-2">
-//                             <form className="space-y-5 bg-white p-6 rounded-lg shadow-2xl">
-//                                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-//                                     <input
-//                                         type="text"
-//                                         name="name"
-//                                         id="name"
-//                                         placeholder="Your Name"
-//                                         required
-//                                         className="w-full p-3 border border-black text-[#1D2B48] border-1 rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-//                                     />
-//                                     <input
-//                                         type="email"
-//                                         name="email"
-//                                         id="email"
-//                                         placeholder="Your Email"
-//                                         required
-//                                         className="w-full p-3 border border-1 border-black text-[#1D2B48] rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-//                                     />
-//                                 </div>
-//                                 <input
-//                                     type="text"
-//                                     name="subject"
-//                                     id="subject"
-//                                     placeholder="Subject"
-//                                     required
-//                                     className="w-full p-3 border border-1 border-black text-[#1D2B48] rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-//                                 />
-//                                 <textarea
-//                                     name="message"
-//                                     id="message"
-//                                     placeholder="Message"
-//                                     required
-//                                     className="w-full p-3 border rounded-md border-1 border-black text-[#1D2B48] h-32 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
-//                                 ></textarea>
-//                                 <div className="text-center">
-//                                     <button
-//                                         type="submit"
-//                                         className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold px-6 py-3 rounded-md shadow-md transition"
-//                                     >
-//                                         Send Message
-//                                     </button>
-//                                 </div>
-//                             </form>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </section>
-//         </main>
-//     );
-// };
+    const res = await insertContact(form);
 
-// export default Page;
+    if (res.success) {
+      toast.success("Message sent successfully!");
+      setForm({ name: "", email: "", subject: "", message: "" });
+    } else {
+      toast.error(res.error || "Failed to send message");
+    }
+  };
 
-function Page() {
+
   return (
     <main>
-      {/* Page Title */}
       <div className="bg-white text-white py-5 -mt-[20px]">
         <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center px-4">
           <h1 className="text-3xl font-bold text-[#1D2B48]">Contact</h1>
         </div>
       </div>
 
-      {/* Contact Section */}
       <section id="contact" className="py-0 bg-white lg:pb-10">
         <div className=" mx-auto px-6  gap-5 grid grid-cols-1 lg:grid-cols-2">
-          {/* Map */}
           <div className="">
             <iframe
               className="w-full h-[400px] rounded-lg shadow-md"
@@ -139,14 +51,16 @@ function Page() {
             ></iframe>
           </div>
 
-          {/* Contact Form */}
           <div className="h-[400px]">
-            <form className="space-y-5 flex-grow flex flex-col bg-white p-6 rounded-lg shadow-2xl h-full">
+            <form
+              onSubmit={handleSubmit}
+              className="space-y-5 flex-grow flex flex-col bg-white p-6 rounded-lg shadow-2xl h-full">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <input
                   type="text"
                   name="name"
-                  id="name"
+                  value={form.name}
+                  onChange={handleChange}
                   placeholder="Your Name"
                   required
                   className="w-full p-3 border border-black text-[#1D2B48] border-1 rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
@@ -154,7 +68,8 @@ function Page() {
                 <input
                   type="email"
                   name="email"
-                  id="email"
+                  value={form.email}
+                  onChange={handleChange}
                   placeholder="Your Email"
                   required
                   className="w-full p-3 border border-1 border-black text-[#1D2B48] rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
@@ -163,14 +78,16 @@ function Page() {
               <input
                 type="text"
                 name="subject"
-                id="subject"
+                value={form.subject}
+                onChange={handleChange}
                 placeholder="Subject"
                 required
                 className="w-full p-3 border border-1 border-black text-[#1D2B48] rounded-md focus:ring-2 focus:ring-indigo-500 focus:outline-none"
               />
               <textarea
                 name="message"
-                id="message"
+                value={form.message}
+                onChange={handleChange}
                 placeholder="Message"
                 required
                 className="w-full p-3 border rounded-md border-1 border-black text-[#1D2B48] h-32 focus:ring-2 focus:ring-indigo-500 focus:outline-none"
@@ -186,10 +103,8 @@ function Page() {
             </form>
           </div>
 
-          {/* Contact Info */}
 
           <div className="space-y-6 p-6 shadow-2xl pt-5 rounded-lg h-[400px] ">
-            {/* <div className="bg-white rounded-lg shadow-2xl p-6 flex flex-col justify-center h-[400px] space-y-6"> */}
             <div>
               <h3 className="text-xl font-semibold text-[#1D2B48] text-center">
                 Get in touch
@@ -233,4 +148,3 @@ function Page() {
   )
 }
 
-export default Page
