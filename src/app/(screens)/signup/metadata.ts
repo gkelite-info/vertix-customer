1b0620e6b15d21e54fd2e369eba6e9e5a287aa1f
gkelite-info <gkeliteinfo@gmail.com>
2025-11-25 13:18:35 +0530

    added seo for signup page

---
+++ src/app/(screens)/signup/metadata.ts
@@ -0,0 +1,14 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Sign Up | Vertix Tax Solutions",
+    description: "Create your Vertix Tax Solutions account to access tax filing tools, resources, and support.",
+    keywords: [
+        "sign up",
+        "create account",
+        "tax solutions",
+        "Vertix tax solutions",
+        "Vertix signup",
+        "tax filing"
+    ],
+};
--- src/app/(screens)/signup/page.tsx
+++ src/app/(screens)/signup/page.tsx
@@ -7,13 +7,7 @@ import { Icon } from "@iconify/react"
 import { supabase } from "../../../../utils/supabase/client"
 import { insertCustomer } from "@/app/api/SupabaseAPI/customer/customerApi"
 import TimezoneSelect from "../../../../utils/timezone"
-import { Metadata } from "next"
 
-export const metadata: Metadata = {
-  title: "Sign Up | Vertix Tax Solutions",
-  description: "Create your Vertix Tax Solutions account to access tax filing tools, resources, and support.",
-  keywords: ["sign up", "create account", "tax solutions", "Vertix tax solutions", "Vertix signup", "tax filing"],
-};
 
 export default function Page() {
   const router = useRouter()
