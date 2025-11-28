93883e0e7d2337c8d4b896f62ceadc746a2bd12f
gkelite-info <gkeliteinfo@gmail.com>
2025-11-28 16:16:16 +0530

    solved glitch of payment breakup

--- src/app/(pageNavbar)/taxfiling/page.tsx
+++ src/app/(pageNavbar)/taxfiling/page.tsx
@@ -5,7 +5,10 @@ export const dynamic = "force-dynamic"
 
 export default function TaxFiling() {
   return (
-    <Suspense fallback={<div>Loading layout...</div>}>
+    <Suspense fallback={
+      <div className="p-4 text-[#1D2B48] bg-white flex flex-col items-center justify-center h-[100vh]">
+        Loading layout...
+      </div>}>
       <TaxfilingContent />
     </Suspense>
   )
